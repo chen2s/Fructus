1476167ae92f97a02445d1e2ca6df024ef78f070
Isaac Hernandez <isaac.rhz@gmail.com>
2020-11-19 16:01:27 -0600

    Create an outstanding settings view

---
+++ Fructus/Views/SettingsLabelView.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsLabelView.swift
+//  Fructus
+//
+//  Created by Isaac Hernandez on 19/11/20.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    
+    var labelText: String
+    var labelImage: String
+    
+    var body: some View {
+        HStack {
+            Text(labelText.uppercased()).fontWeight(.bold)
+            Spacer()
+            Image(systemName: labelImage)
+        } // : HStack
+    }
+}
+
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
