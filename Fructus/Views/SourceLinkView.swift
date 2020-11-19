10f1348d28c914b41b4fea431804c64080dd9f31
Isaac Hernandez <isaac.rhz@gmail.com>
2020-11-19 12:53:26 -0600

    Detail view Part 1

---
+++ Fructus/Views/SourceLinkView.swift
@@ -0,0 +1,30 @@
+//
+//  SourceLinkView.swift
+//  Fructus
+//
+//  Created by Isaac Hernandez on 19/11/20.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    var body: some View {
+        GroupBox(){
+            HStack{
+                Text("Content source")
+                Spacer()
+                Link("Wikipedia", destination: URL(string: "https://wikipedia.com")!)
+                Image(systemName: "arrow.up.right.square")
+            } //: HStack
+            .font(.footnote)
+        } //: GroupBox
+    }
+}
+
+struct SourceLinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        SourceLinkView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
